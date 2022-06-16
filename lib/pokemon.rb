62a2f95caeff18d81a7c20ca2da594752cf7292e
Brian Sitati <briansit93@gmail.com>
2022-06-16 18:08:28 +0300

    commit tree b5e5e176a80979fa092a4ccb697121703b87f22e

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
 class Pokemon
-end
+
+    attr_accessor :id, :name, :type, :db
+  
+    def initialize(id:, name:, type:, db:)
+      @id = id
+      @name = name
+      @type = type
+      @db = db
+    end
+  
+    def self.save(name, type, db)
+      sql = "iNSERT INTO pokemon (name, type) values (?, ?)"
+      db.execute(sql, name, type)
+    end
+  
+    def self.find(id, db)
+      sql = "SELECT * FROM pokemon WHERE id=?"
+      # binding.pry
+      result = db.execute(sql, id).flatten
+      Pokemon.new(id: result[0], name: result[1], type: result[2], db: db)
+    end
+    
+  end
